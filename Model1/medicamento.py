9ba25efe456d642f63fc4358868f4aa1e8f5f8c9
Monserrath Rojas <78930151+DisSisCamachoROsorioRPalacios@users.noreply.github.com>
2022-11-26 18:01:03 -0600

    Update medicamento.py

--- Model1/medicamento.py
+++ Model1/medicamento.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-from gestion stock import gestion stock
+from gestion_stock import gestion_stock
 
-class medicamento(gestion stock):
-    def __init__(self):
+class medicamento(gestion_stock):
+    def __init__(self,idMedicamento,ubicacion,nombre,cantidad):
+        gestion_stock.__init__(self,idMedicamento,ubicacion,cantidad)
         self.idMedicamento = None
         self.nombre = None
         self.cantidad = None
 
     def curar(self, ):
-        pass
+        print("El paciente tomo elmedicamento correctamente")
 
     def caducar(self, ):
-        pass
+        print("El medicamento ha caducado ")
+    
+    def __toString__(self):
+        return gestion_stock.__toString__(self)+"idMedicamento:{} Nombre : {} ubicacion:{} cantidad:{}".format(self.idMedicamento,self.nombre,self.ubicacion,self.cantidad)
 
+medica= medicamento(1,"zumpango","paracetamol",12)
+print(medica.__toString__())
